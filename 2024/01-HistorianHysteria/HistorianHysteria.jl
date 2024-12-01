b6abdcfe657b5888300331af6f31c64e9fdacb91
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2024-12-01 12:10:20 -0500

    2024 Day 1 Add benchmarking

--- 2024/01-HistorianHysteria/HistorianHysteria.jl
+++ 2024/01-HistorianHysteria/HistorianHysteria.jl
@@ -49,10 +49,20 @@ function count_occurrences(input::Vector{Int})::Dict{Int, Int}
 end
 
 function main(filename::String)
-    input = read_input(filename)
-    input = parse_input(input)
-    println("Part 1: ", solve_part1(input))
-    println("Part 2: ", solve_part2(input))
+    parse_start = time_ns()
+    input = parse_input(read_input(filename))
+    parse_ms = (time_ns() - parse_start) / 1.0e6
+    println("Parse time: ", parse_ms, " ms")
+
+    part1_start = time_ns()
+    part1 = solve_part1(input)
+    part1_ms = (time_ns() - part1_start) / 1.0e6
+    println("Part 1: ", part1, " (", part1_ms, " ms)")
+
+    part2_start = time_ns()
+    part2 = solve_part2(input)
+    part2_ms = (time_ns() - part2_start) / 1.0e6
+    println("Part 2: ", part2, " (", part2_ms, " ms)")
 end
 
 filename = length(ARGS) > 0 ? ARGS[1] : "sample_input.txt"
