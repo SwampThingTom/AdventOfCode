b942523518ace8d7c9c54125edc4108103afa215
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2025-11-30 21:39:36 -0500

    2025 setup

---
+++ 2025/README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2025 Solutions
+
+My solutions for the [2025 Advent of Code](https://adventofcode.com/2025) in Swift.
---
+++ 2025/template/Code.swift
@@ -0,0 +1,52 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+// <name>
+// https://adventofcode.com/2024/day/<day>
+
+typealias InputType = [String]
+typealias SolutionType = Int
+
+func readInput(filename: String) -> String {
+    try! String(contentsOfFile: filename, encoding: .utf8).trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+func parse(input: String) -> InputType {
+    input.components(separatedBy: "\n")
+}
+
+func solvePart1(input: InputType) -> SolutionType {
+    // TODO: Implement part 1
+    0
+}
+
+func solvePart2(input: InputType) -> SolutionType {
+    // TODO: Implement part 2
+    0
+}
+
+func main(_ filename: String) {
+    let clock = ContinuousClock()
+    
+    var input: InputType!
+    let parseDuration = clock.measure {
+        input = parse(input: readInput(filename: filename))
+    }
+    print("Parse time: \(parseDuration / .milliseconds(1)) ms")
+
+    var part1: SolutionType!
+    let part1Duration = clock.measure {
+        part1 = solvePart1(input: input)
+    }
+    print("Part 1: \(part1!) (\(part1Duration / .milliseconds(1)) ms)")
+
+    var part2: SolutionType!
+    let part2Duration = clock.measure {
+        part2 = solvePart2(input: input)
+    }
+    print("Part 2: \(part2!) (\(part2Duration / .milliseconds(1)) ms)")
+}
+
+let filename = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : "sample_input.txt"
+main(filename)
